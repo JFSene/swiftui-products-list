4cf0f0392f9e6c394fb9eca13943c0134403bdb6
JFSene <joel.sene@hotmail.com>
2024-05-25 15:06:47 +0100

    Initial Commit

---
+++ DevoteamTest/DevoteamTestApp.swift
@@ -0,0 +1,32 @@
+//
+//  DevoteamTestApp.swift
+//  DevoteamTest
+//
+//  Created by Joel Sene on 25/05/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DevoteamTestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
